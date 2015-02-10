0e1624d2e9e3f62a6b09ce39771f1a19f83b30a5
djtfmartin <djtfmartin@gmail.com>
2015-02-10 12:35:53 +1100

    polygon merge

--- scripts/spark.sh
+++ scripts/spark.sh
@@ -1,24 +1,32 @@
 #!/bin/sh
 # Launch a cluster
-./spark-ec2 --slaves=4 --key-pair=dmartin-spark-amazon --identity-file=~/.ssh/dmartin-spark-amazon.pem --region=ap-northeast-1 launch timeseries
+date
+./spark-ec2 --slaves=4 --key-pair=amazon-sydney --identity-file=/Users/mar759/.ssh/amazon-sydney.pem --region=ap-southeast-2 launch timeseries
+date
+
 
 # Load a file into HDFS
-cd /root/ephemeral-hdfs/bin
-./hadoop fs -mkdir /raw
-./hadoop fs -mkdir /java
-./hadoop fs -mkdir /output
-./hadoop fs -put /root/aves.csv /raw/
-./hadoop fs -put /root/ts-hulls-1.0-SNAPSHOT-jar-with-dependencies.jar /java/
-./hadoop fs -ls /raw
+/root/ephemeral-hdfs/bin/hadoop fs -mkdir /raw
+/root/ephemeral-hdfs/bin/hadoop fs -mkdir /java
+/root/ephemeral-hdfs/bin/hadoop fs -mkdir /output
+/root/ephemeral-hdfs/bin/hadoop fs -put /root/aves.csv /raw/
+/root/ephemeral-hdfs/bin/hadoop fs -ls /raw
+
+#./hadoop fs -put /root/ts-hulls-1.0-SNAPSHOT-jar-with-dependencies.jar /root/
 
 # Run the job
-/root/spark/bin/spark-submit --class au.org.ala.timeseries.GeneratePolygons --master spark://54.66.44.117:7077  /root/ts-hulls-1.0-SNAPSHOT-jar-with-dependencies.jar
+/root/spark/bin/spark-submit --class au.org.ala.timeseries.GeneratePolygons --master spark://ec2-NAME-IN_HERE:7077  /root/ts-hulls-1.0-SNAPSHOT-jar-with-dependencies.jar hdfs:///raw/aves.csv
 
 # Should output the following
 # 15/02/01 11:06:57 INFO scheduler.DAGScheduler: Job 0 finished: saveAsTextFile at GeneratePolygons.scala:53, took 1447.851482 s
 
 # Merge output
-./hadoop fs -cat  /user/root/polygons/part* | ./hadoop fs -put - /output/all.csv
+/root/ephemeral-hdfs/bin/hadoop fs -cat  /user/root/polygons/part* | /root/ephemeral-hdfs/bin/hadoop fs -put - /output/all.csv
 
 # Retrieve output
-./hadoop fs -copyToLocal /output/all.csv /root/all.csv
+/root/ephemeral-hdfs/bin/hadoop fs -copyToLocal /output/all.csv /root/all.csv
+
+# Remove from HDFS
+/root/ephemeral-hdfs/bin/hadoop fs -rmr  /user/root/polygons
+
+#grep "POLYGON" all.csv | sort > all-sorted.csv
---
+++ src/main/scala/au/org/ala/timeseries/GeneratePolygons.scala
@@ -0,0 +1,178 @@
+package au.org.ala.timeseries
+
+import com.vividsolutions.jts.algorithm.ConvexHull
+import com.vividsolutions.jts.geom.{GeometryCollection, Coordinate, GeometryFactory, Geometry}
+import org.apache.commons.math3.ml.clustering.{DoublePoint, DBSCANClusterer}
+import org.apache.spark.{SparkContext, SparkConf}
+import org.geotools.filter.AreaFunction
+import org.geotools.geometry.jts.JTS
+import com.vividsolutions.jts.io.WKTWriter
+import scala.collection.mutable.{ListBuffer,HashSet}
+import scala.collection.JavaConversions._
+
+/**
+ * Apache Spark job for calculating polygons.
+ *
+ *
+ */
+object GeneratePolygons {
+
+  def main(args:Array[String]): Unit = {
+
+    if(args.length != 1){
+      println("Please supply a local file path or HDFS path.")
+      return
+    }
+
+    val conf = new SparkConf().setAppName("Species Time-series Polygons")
+    val sc = new SparkContext(conf)
+
+    // /Users/mar759/Downloads/small-insecta.csv
+//    val lines = sc.textFile("/Users/mar759/Downloads/small-insecta.csv")
+    val lines = sc.textFile(args(0))
+
+    val scientificName_idx = 2
+    val family_idx = 3
+    val decimalLatitude_idx = 4
+    val decimalLongitude_idx = 5
+    val year_idx = 8
+
+    //filter points without a year
+    val nameLatLngYr = lines.map(line => {
+      val parts = line.split("\t")
+      if(parts.length == 9){
+        //Family|scientificName, year, latitude, longitude, spatiallyValid
+        List(
+          List(parts(family_idx), parts(scientificName_idx)).mkString("|"),
+          parts(year_idx), //year
+          parts(decimalLatitude_idx), //latitude
+          parts(decimalLongitude_idx) //longitude
+        )
+      } else {
+        List("","","","")
+      }
+    })
+
+    //create the collections -  org.apache.spark.rdd.RDD[(String, Iterable[(String, String, String, String)])]
+    val groupsByNameAndYear = nameLatLngYr.groupBy(groupByNameAndDecade)
+
+    //map this to sciName+year, count for now
+    //compute the hull for a year
+    val nameAndCounts = groupsByNameAndYear.map(createPolygons)
+    nameAndCounts.saveAsTextFile("polygons")
+  }
+
+  //nameLatLngYr.saveAsTextFile("nameLatLngYr"
+  //map to set of collections
+  //name, year, lat, lng
+  def groupByNameAndYear(x:List[String]) = x(0) + "\t" + x(1)
+
+  def groupByNameAndDecade(x:List[String]) = {
+    if(x(1) != ""){
+      val year = (x(1).toInt / 10) * 10
+      x(0) + "\t" + year
+    } else {
+      x(0) + "\t" + "noyear"
+    }
+  }
+
+  def createPolygons(sciNameYearGroup: (String, Iterable[List[String]])) : String = {
+
+    val (sciNameYr, records) = sciNameYearGroup
+
+    val doublePoints = new HashSet[DoublePoint]
+
+    //val source = Source.fromFile("/data/thornbill.csv").getLines().foreach { line =>
+    records.foreach { record =>
+      if(record(2) != "" && record(3) != ""){
+        doublePoints += new DoublePoint(Array[Double](record(3).toDouble, record(2).toDouble))
+      }
+    }
+
+    val start = System.currentTimeMillis()
+
+    //generate clusters...
+    val clusterer = new DBSCANClusterer[DoublePoint](0.5, 10)
+
+    val listOfClusters = clusterer.cluster(doublePoints.toList)
+
+    //generate convex hulls
+    val generatedPolygons = listOfClusters.map(cluster => {
+
+      val buff = new ListBuffer[Coordinate]
+      //longitude, latitude
+      cluster.getPoints.foreach(doublePoint => buff += new Coordinate(doublePoint.getPoint()(0), doublePoint.getPoint()(1)))
+
+      //generate convex hull
+      val convexHull = new ConvexHull(buff.toArray, new GeometryFactory())
+      val geom = convexHull.getConvexHull
+
+      //generate smooth polygons
+      JTS.smooth(geom, 0.2)
+    })
+
+    //check for intersections and merge
+    val mergedPolygons = mergeIfOverlaps(generatedPolygons)
+
+    //wkts
+    val wktWriter = new WKTWriter()
+    val wkts = new ListBuffer[String]()
+
+    val areaFunction = new AreaFunction()
+    val areas = new ListBuffer[Double]()
+
+    //write out polygons
+    mergedPolygons.foreach { polygon =>
+      val wkt = wktWriter.write(polygon)
+      wkts += wkt
+      areas += areaFunction.getArea(polygon)
+    }
+
+    sciNameYr + "\t" + wkts.mkString("|") + "\t" + areas.mkString("|")
+  }
+
+  /**
+   * Returns an option which is a non empty tuple if theres an overlap
+   * @param geoms
+   * @return
+   */
+  def hasOverlap(geoms:Seq[Geometry]) : Option[(Geometry, Geometry)] = {
+    geoms.foreach( geom1 => {
+      geoms.foreach( geom2 => {
+        if(geom1 != geom2 && geom1.isInstanceOf[com.vividsolutions.jts.geom.Polygon] && geom1.isInstanceOf[com.vividsolutions.jts.geom.Polygon] && geom1.intersects(geom2)) {
+          val merged = geom1.union(geom2)
+          if(!merged.isInstanceOf[GeometryCollection]){
+            return Some((geom1, geom2))
+          }
+        }
+      })
+    })
+    None
+  }
+
+  /**
+   * Recursive merge of geometries.
+   * @param geoms
+   * @return merged geometries
+   */
+  def mergeIfOverlaps(geoms:Seq[Geometry]): Seq[Geometry] = {
+    val overlappers = hasOverlap(geoms)
+    if(!overlappers.isEmpty){
+      val (geom1, geom2) = overlappers.get
+      val theNewGeom = {
+        val merged = geom1.union(geom2)
+        if(merged.isInstanceOf[GeometryCollection]){
+          merged.convexHull()
+        } else {
+          merged
+        }
+      }
+
+      //remove the source geoms, add the new
+      val mergedGeoms = geoms.diff(Seq(geom1, geom2)) ++ Seq(theNewGeom)
+      mergeIfOverlaps(mergedGeoms)
+    } else {
+      geoms
+    }
+  }
+}
